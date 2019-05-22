99c84e6ff4839e4acd1832dae7aa6f6f2d12aeb2
Venkatesh-Prasad Ranganath <rvprasad@ksu.edu>
2019-05-22 18:41:58 -0500

    Adds Cyclone based server
    Fixes #1

---
+++ servers/cyclone/server.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python2.7 server.py
+
+import cyclone.web
+import json
+import math
+import random
+import sys
+from timeit import default_timer as timer
+
+from twisted.internet import reactor
+from twisted.python import log
+
+
+class RandomNumberGenerator(cyclone.web.RequestHandler):
+    def get(self):
+        num = int(self.get_argument('num')) if self.get_argument('num') else 10
+        randoms = map(lambda x: math.floor(random.random() * 1e6),
+                      range(0, num))
+        self.write(json.dumps(randoms, separators=(',', ':')))
+        stop = timer()
+
+
+if __name__ == "__main__":
+    application = cyclone.web.Application([
+        (r"/random", RandomNumberGenerator)
+    ])
+
+    log.startLogging(sys.stdout)
+    reactor.listenTCP(1234, application)
+    reactor.run()
