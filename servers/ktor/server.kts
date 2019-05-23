05f81a478ebd6a24b0c17a0932e3900719004379
Venkatesh-Prasad Ranganath <rvprasad@ksu.edu>
2019-05-23 16:39:27 -0500

    Adds Ktor based server
    Fixes #3

---
+++ servers/ktor/server.kts
@@ -0,0 +1,33 @@
+// kscript server.kts
+
+@file:CompilerOpts("-jvm-target 1.8")
+@file:DependsOn("io.ktor:ktor-server-netty:1.2.0")
+@file:DependsOn("com.google.code.gson:gson:2.8.5")
+@file:DependsOn("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.3.31")
+@file:KotlinOpts("-J-Xmx1024M")
+
+import com.google.gson.Gson
+import io.ktor.application.call
+import io.ktor.http.ContentType
+import io.ktor.response.respondText
+import io.ktor.routing.get
+import io.ktor.routing.routing
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.netty.Netty
+import java.util.Random
+
+val server = embeddedServer(Netty, 1234, "0.0.0.0") {
+  routing {
+    get("/random") {
+      val start = System.nanoTime()
+      val num = call.request.queryParameters["num"]?.toInt() ?: 10
+      val randoms =  Random().ints(num.toLong()).toArray()
+      val gson = Gson()
+      call.respondText(gson.toJson(randoms), ContentType.Text.Plain)
+      val duration = System.nanoTime() - start
+      println("%1$5.3fms".format(duration / 1e6))
+    }
+  }
+}
+
+server.start(wait = true)
