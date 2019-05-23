34eb15655871726f25d8731c5eda6fd2e560bae9
Venkatesh-Prasad Ranganath <rvprasad@ksu.edu>
2019-05-22 21:27:42 -0500

    Changes Erlang client to verify request was successfully
    Fixes #9

--- clients/http-erlang/client.erl
+++ clients/http-erlang/client.erl
@@ -7,20 +7,24 @@
 
 dispatch_request(Url, Parent) ->
   Start = erlang:monotonic_time(millisecond),
-  {Status, _} = httpc:request(Url),
-  Suffix =
-    case Status of
-      ok -> 'OK';
-      error -> 'ERR'
-    end,
-  io:format("~pms ~s~n",
-    [(erlang:monotonic_time(millisecond) - Start), Suffix]),
-  Parent ! done.
+  {Status, Value} = httpc:request(Url),
+  Elapsed_Time = erlang:monotonic_time(millisecond) - Start,
+  case Status of
+    ok ->
+      io:format("~pms OK~n", [Elapsed_Time]),
+      Parent ! succ;
+    error ->
+      io:format("~pms REQ_ERR ~p~n", [Elapsed_Time, element(1, Value)]),
+      Parent ! fail
+  end.
 
-wait_on_children(0) -> return;
-wait_on_children(Num) ->
+wait_on_children(0, NumOfSucc, NumOfFail) ->
+  io:format("Success: ~p~n", [NumOfSucc]),
+  io:format("Failure: ~p~n", [NumOfFail]);
+wait_on_children(Num, NumOfSucc, NumOfFail) ->
   receive
-    done -> wait_on_children(Num - 1)
+    succ -> wait_on_children(Num - 1, NumOfSucc + 1, NumOfFail);
+    fail -> wait_on_children(Num - 1, NumOfSucc, NumOfFail + 1)
   end.
 
 main(Args) ->
@@ -29,4 +33,4 @@ main(Args) ->
   Num = list_to_integer(lists:nth(2, Args)),
   Parent = self(),
   [spawn(fun() -> dispatch_request(Url, Parent) end) || _ <- lists:seq(1, Num)],
-  wait_on_children(Num).
+  wait_on_children(Num, 0, 0).
