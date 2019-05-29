29a1568a5f52b61db0cc6eb0d9e4fd22c19cb274
Venkatesh-Prasad Ranganath <venkateshprasad.ranganath@gmail.com>
2019-05-29 13:36:46 -0500

    Fixes bug due to use of input num as a string
    Converts the num into an integer

--- servers/yaws-erlang/random_server.erl
+++ servers/yaws-erlang/random_server.erl
@@ -12,11 +12,11 @@ to_string(X) -> io_lib:format("\"~6..0B\"", [X]).
 out(Req) ->
   Start = erlang:monotonic_time(microsecond),
   Num = case yaws_api:queryvar(Req, "num") of
-    {ok, Val} -> Val;
+    {ok, Val} -> list_to_integer(Val);
     _ -> 10
   end,
   RandomNums = random_generator(Num, []),
-  Reply = "[" ++ string:join(lists:map(fun to_string/1, RandomNums), ",") 
+  Reply = "[" ++ string:join(lists:map(fun to_string/1, RandomNums), ",")
     ++ "]",
   Elapsed_Time = (erlang:monotonic_time(microsecond) - Start) / 1000,
 	io:fwrite("~pms~n", [Elapsed_Time]),
