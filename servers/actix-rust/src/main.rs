36220aa24d2350cce49ce2b462bc50fa37773790
Venkatesh-Prasad Ranganath <venkateshprasad.ranganath@gmail.com>
2019-06-02 01:01:40 -0500

    Adds code to collect timing info

--- servers/actix-rust/src/main.rs
+++ servers/actix-rust/src/main.rs
@@ -3,12 +3,14 @@ extern crate rand;
 extern crate serde_json;
 
 use actix_web::{server, App, HttpRequest};
+use std::time::{Instant};
 
 fn main() {
   server::new(|| App::new().resource("/random", |r| r.f(random_handler)))
     .bind("0.0.0.0:1234").unwrap().run();
 
   fn random_handler(req: &HttpRequest) -> String {
+    let start = Instant::now();
 
     let num = match req.query().get("num") {
       Some(n) => n.parse::<i32>().unwrap(),
@@ -17,7 +19,10 @@ fn main() {
 
     let rands: Vec<String> = get_randoms(num).iter()
       .map(|i| format!("{:06}", i)).collect();
-    return serde_json::to_string(&rands).unwrap();
+    let ret = serde_json::to_string(&rands).unwrap();
+    let elapsed_time = Instant::now().duration_since(start).as_micros();
+    println!("{:0.3}ms", (elapsed_time as f32) / 1000.0);
+    return ret;
   }
 
   fn get_randoms(num: i32) -> Vec<i32> {
