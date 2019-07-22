58fd834c83dc73ea6331d03ef6efa1b6ce7bc399
Venkatesh-Prasad Ranganath <venkateshprasad.ranganath@gmail.com>
2019-07-21 20:15:56 -0500

    Formatting

--- servers/cowboy-erlang/src/random_handler.erl
+++ servers/cowboy-erlang/src/random_handler.erl
@@ -12,11 +12,11 @@ to_string(X) -> io_lib:format("\"~6..0B\"", [X]).
 
 init(Request=#{method := <<"GET">>}, State) ->
   Start = erlang:monotonic_time(microsecond),
-	#{num := Num} = cowboy_req:match_qs([{num, int, 10}], Request),
+  #{num := Num} = cowboy_req:match_qs([{num, int, 10}], Request),
   RandomNums = random_generator(Num, []),
   Ret = "[" ++ string:join(lists:map(fun to_string/1, RandomNums), ",") ++ "]",
   Reply = cowboy_req:reply(200,
     #{<<"content-type">> => <<"text/plain">>}, Ret, Request),
   Elapsed_Time = (erlang:monotonic_time(microsecond) - Start) / 1000,
-	io:fwrite("~pms~n", [Elapsed_Time]),
-	{ok, Reply, State}.
+  io:fwrite("~pms~n", [Elapsed_Time]),
+  {ok, Reply, State}.
