8f584fff2f717ac2fbbdc2f82a7845e9c55bcc8f
Venkatesh-Prasad Ranganath <venkateshprasad.ranganath@gmail.com>
2019-11-14 02:42:54 -0600

    Change network traffic configurations

--- scripts/generate_reqs_payload_config.py
+++ scripts/generate_reqs_payload_config.py
@@ -1,6 +1,6 @@
 
 conc_reqs = [100, 500, 1000, 1500, 2500]
-network_traffic = [2e6, 5e6, 1e7]
+network_traffic = [2e6, 4e6]
 
 # 5 is the number of worker nodes
 # 9 is the number of bytes occupied by each number on the wire
