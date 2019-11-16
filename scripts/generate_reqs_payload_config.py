1a299160e5bbd9ac97f393c36f51dd0a196f560c
Venkatesh-Prasad Ranganath <venkateshprasad.ranganath@gmail.com>
2019-11-15 21:52:37 -0600

    Update request and payload configurations

--- scripts/generate_reqs_payload_config.py
+++ scripts/generate_reqs_payload_config.py
@@ -1,6 +1,6 @@
 
 conc_reqs = [100, 500, 1000, 1500, 2500]
-network_traffic = [2e6, 4e6]
+network_traffic = [2e6, 6e6, 10e6]
 
 # 5 is the number of worker nodes
 # 9 is the number of bytes occupied by each number on the wire
